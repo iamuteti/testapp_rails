bf1a30875aaa2a14ba1ced33c211ca49bbe33b3f
John Muteti <iamuteti@gmail.com>
2016-06-02 11:02:46 +0300

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # Change these
-server '178.62.88.94', port: 7171, roles: [:web, :app, :db], primary: true
+server '162.243.17.252', port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:sheharyarn/testapp_rails.git'
+set :repo_url,        'https://github.com/iamuteti/testapp_rails.git'
 set :application,     'testapp'
-set :user,            'deploy'
+set :user,            'john'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
